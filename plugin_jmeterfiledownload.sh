9c57ef6d78b02c80656797fc4cb1fd27fb063cec
Stephen Sigwart <ssigwart@gmail.com>
2018-06-23 11:15:08 -0400

    File download plugin.

---
+++ plugin_jmeterfiledownload.sh
@@ -0,0 +1,37 @@
+# Plugin install script.  Download and extracts files.
+function plugin_install()
+{
+	# Get user info
+	userHome=$1
+	user=$2
+
+	# Get file to download
+	url=$(plugin_setting jmeterfiledownload_filename)
+	if [ "${url}" != "" ]; then
+		# Set up directory
+		cd "${userHome}"
+		if [ ! -d downloads ]; then
+			mkdir downloads
+			chown "${user}:${user}" downloads
+		fi
+		cd downloads
+		# Download
+		rl_logger "Downloading ${url}"
+		file=$(basename "${url}")
+		curl "${url}" > "${file}"
+		chown "${user}:${user}" "${file}"
+		if [ $? -ne 0 ]; then
+			rl_logger "Failed to download ${url}"
+		else
+			# Extra file if desired
+			tarExtract=$(plugin_setting jmeterfiledownload_tar_extract)
+			if [ "${tarExtract}" = "T" ]; then
+				rl_logger "Extracting ${file}"
+				sudo -u "${user}" tar xf "${file}"
+				if [ $? -ne 0 ]; then
+					rl_logger "Failed to extract file"
+				fi
+			fi
+		fi
+	fi
+}
